d989a5614e9262b27cc8d0ae425fb430cb3d3e97
Zenara Daley <zrdaley@gmail.com>
2018-07-17 19:29:16 -0700

    add output log to run_ministat.sh

--- run_minisat.sh
+++ run_minisat.sh
@@ -2,6 +2,8 @@
 
 rm -r minisat_puzzle_output
 mkdir minisat_puzzle_output
+rm minisat_puzzle_logs.txt
 for filename in sud2sat_puzzle_output/*; do
-  minisat "$filename" "minisat_puzzle_output/`basename $filename`"
+  minisat "$filename" "minisat_puzzle_output/`basename $filename`" | grep "CPU time" >> minisat_puzzle_logs.txt
+  sleep 0.1
 done

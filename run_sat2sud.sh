9048af2942b8bc9f428fb4b1f771b7dc4be9e879
Zenara Daley <zrdaley@gmail.com>
2018-08-03 11:39:59 -0700

    start grids at 1, not 0

--- run_sat2sud.sh
+++ run_sat2sud.sh
@@ -3,7 +3,7 @@
 rm solved_puzzles.txt
 touch solved_puzzles.txt
 
-i=0
+i=1
 for filename in minisat_puzzle_output/*; do
   echo "Grid $i" >> solved_puzzles.txt
   ./sat2sud "$filename" >> solved_puzzles.txt

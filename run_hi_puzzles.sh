88d9cc176d096ef0974058aaaf8a74b8a62cac6e
chrisann0295 <chrisann@uvic.ca>
2018-08-05 17:34:25 -0700

    Adding scripting for running hi puzzles

---
+++ run_hi_puzzles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+rm -r hi_puzzles
+mkdir hi_puzzles
+split -l 1 puzzles_hi_encoding.txt hi_puzzles/puzzle
+cd hi_puzzles
+
+for filename in *; do
+  tail  "$filename" > "$filename.tmp" && mv "$filename.tmp" "$filename"
+done
+
+cd ..
+rm -r sud2sat_hi_puzzle_output
+mkdir sud2sat_hi_puzzle_output
+for filename in puzzles/*; do
+  ./sud2sat "$filename" "sud2sat_hi_puzzle_output/`basename $filename`"
+done
